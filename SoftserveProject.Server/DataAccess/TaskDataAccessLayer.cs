83b86bbe91c892c003b74a813c1c6c484f9537b6
ALMLAMBO <aleksm007148@gmail.com>
2019-07-11 17:04:29 +0300

    Add task data access layer

---
+++ SoftserveProject.Server/DataAccess/TaskDataAccessLayer.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftserveProject.Server.DataAccess {
+    public class TaskDataAccessLayer {
+        private string projectId;
+        FirestoreDb firestoreDb;
+
+        public TaskDataAccessLayer() {
+            string filepath = "C:\\Users\\AAA\\Desktop\\softserve-app-5dab0-90c2a616e393.json";
+
+            Environment.SetEnvironmentVariable(
+                "GOOGLE_APPLICATION_CREDENTIALS", filepath);
+
+            projectId = "softserve-app";
+            firestoreDb = FirestoreDb.Create(projectId);
+        }
+    }
+}
