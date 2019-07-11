04503e5959a81af2fb987fb170e5028155338126
ALMLAMBO <aleksm007148@gmail.com>
2019-07-11 17:58:44 +0300

    Add description for firestore property and make it public

--- SoftserveProject.Server/DataAccess/TaskDataAccessLayer.cs
+++ SoftserveProject.Server/DataAccess/TaskDataAccessLayer.cs
@@ -1,13 +1,15 @@
 ﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SoftserveProject.Shared.Models;
 
 namespace SoftserveProject.Server.DataAccess {
     public class TaskDataAccessLayer {
         private string projectId;
-        FirestoreDb firestoreDb;
+        private FirestoreDb firestoreDb;
 
         public TaskDataAccessLayer() {
             string filepath = "C:\\Users\\AAA\\Desktop\\softserve-app-5dab0-90c2a616e393.json";
@@ -17,6 +19,36 @@ namespace SoftserveProject.Server.DataAccess {
 
             projectId = "softserve-app";
             firestoreDb = FirestoreDb.Create(projectId);
+        }
+
+        public async Task<List<TodoTask>> GetAllTasks() {
+            Query todoTaskQuery = firestoreDb.Collection("tasks");
+            QuerySnapshot todoTaskQuerySnapshot = await
+                todoTaskQuery.GetSnapshotAsync();
+
+            List<TodoTask> todoTasks = new List<TodoTask>();
+
+            try {
+                foreach (DocumentSnapshot ds in 
+                    todoTaskQuerySnapshot.Documents) {
+
+                    if(ds.Exists) {
+                        TodoTask newTodoTask = ds
+                            .ConvertTo<TodoTask>();
+
+                        todoTasks.Add(newTodoTask);
+                    }
+
+                    return todoTasks
+                        .OrderBy(x => x.CreatedAt)
+                        .ToList();
+                }
+            } 
+            catch {
+                throw;
+            }
+
+            return todoTasks;
         }
     }
 }
--- SoftserveProject.Shared/Models/TodoTask.cs
+++ SoftserveProject.Shared/Models/TodoTask.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace SoftserveProject.Shared.Models {
     [FirestoreData]
-    class TodoTask {
+    public class TodoTask {
         public string Id { get; set; }
         [FirestoreProperty]
         public DateTime CreatedAt { get; set; }
         [FirestoreProperty]
         public string TaskName { get; set; }
+        [FirestoreProperty]
         public string Description { get; set; }
     }
 }
